b3168c90d459f4d6b3001b0e7e0e0e2b822e037a
HemaSree1519 <khemasree96@gmail.com>
2019-05-08 00:00:35 +0530

    Expand the input for when started to write notes

--- src/Containers/NotesBoard/index.js
+++ src/Containers/NotesBoard/index.js
@@ -19,8 +19,9 @@ export default class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes: [1, 2],
-            edit: false
+            notes: [1, 2, 3],
+            edit: false,
+            isWritingNote: false
         }
     }
 
@@ -30,12 +31,30 @@ export default class Index extends Component {
             edit: !prevState.edit
         }))
     };
+    onWrite = () => {
+        this.setState({
+            isWritingNote: !this.state.isWritingNote
+        })
+    };
 
     render() {
         return (
-            <div className="Home">
-                <div className="lander">
-                    <Input className="input" type="text" placeholder="Write a note..."/>
+            <div className="dash-board">
+                <div className="write-note">
+                    {!this.state.isWritingNote ?
+                        <Input className="note-input" type="text" placeholder="Write a note..." onClick={() => {
+                            this.onWrite();
+                        }}/>
+                        :
+                        <Card className="note-input">
+                            <Input className="note-card-input" type="text" placeholder="Title"/>
+                            <Input className="note-card-input" type="textarea" placeholder="Write a note..."/>
+                            <div className="footer-container">
+                                <Button className="footer-button" onClick={() => this.onWrite()}>Close</Button>
+                            </div>
+                        </Card>
+                    }
+
                 </div>
                 <div>
                     {!this.state.notes.length > 0 ?
@@ -50,7 +69,7 @@ export default class Index extends Component {
                                             this.onEdit(true);
                                         }}>
                                             <CardBody>
-                                                <CardTitle className="title">Note-1</CardTitle>
+                                                <CardTitle className="card-title">Note-1</CardTitle>
                                                 <CardText>This is a wider card with supporting.</CardText>
                                             </CardBody>
                                         </Card>
@@ -64,17 +83,16 @@ export default class Index extends Component {
                         <ModalHeader className="modal-header">Modal title</ModalHeader>
                         <ModalBody>
                             <Input type="textarea"
-                                   className="modal-input"
+                                   className="note-card-input"
                                    value="Write something (data should remain in modal if unmountOnClose is set to false)"
                                    rows={5}/>
                             <br/>
-                            <div className="footer-label">
-                                <Label className="footer-edited">Edited 3:33 PM</Label>
+                            <div className="footer-container">
+                                <Label className="footer-edited-time">Edited 3:33 PM</Label>
                             </div>
                         </ModalBody>
                         <ModalFooter className="modal-footer">
-                            <Button className="footer-button" onClick={this.onEdit}>Cancel</Button>{'     '}
-                            <Button className="footer-button" onClick={this.onEdit}>Save</Button>
+                            <Button className="footer-button" onClick={this.onEdit}>Close</Button>
                         </ModalFooter>
                     </Modal>
                 </div>
