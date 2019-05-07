cd8de17ef585024864573788ea67cbb0c765d432
HemaSree1519 <khemasree96@gmail.com>
2019-05-07 22:47:30 +0530

    Display the list of notes through map

--- src/Containers/NotesBoard/index.js
+++ src/Containers/NotesBoard/index.js
@@ -1,14 +1,36 @@
 import React, {Component} from "react";
 import "./style.css"
-import {Card, CardBody, CardColumns, CardText, CardTitle, Input} from "reactstrap";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardColumns,
+    CardText,
+    CardTitle,
+    Input,
+    Label,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader
+} from "reactstrap";
 
 export default class Index extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            notes: [1]
+        this.state = {
+            notes: [1, 2],
+            edit: false
         }
     }
+
+    onEdit = () => {
+        console.log("Editable");
+        this.setState(prevState => ({
+            edit: !prevState.edit
+        }))
+    };
+
     render() {
         return (
             <div className="Home">
@@ -16,42 +38,45 @@ export default class Index extends Component {
                     <Input className="input" type="text" placeholder="Write a note..."/>
                 </div>
                 <div>
-                    {!this.state.notes.length>0 ?
+                    {!this.state.notes.length > 0 ?
                         <p>"Get your note saved with Save Note app"</p>
                         :
                         <CardColumns>
-                            <Card>
-                                <CardBody>
-                                    <CardTitle className="title">Note-1</CardTitle>
-                                    <CardText>This is a wider card with supporting.</CardText>
-                                </CardBody>
-                            </Card>
-                            <Card>
-                                <CardBody>
-                                    <CardTitle className="title">Card title</CardTitle>
-                                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                                </CardBody>
-                            </Card>
-                            <Card>
-                                <CardBody>
-                                    <CardTitle className="title">Card title</CardTitle>
-                                    <CardText>This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This card has even longer content than the first to
-                                        show that equal height action. This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This card has even longer content than the first to
-                                        show that equal height action.</CardText>
-                                </CardBody>
-                            </Card>
-                            <Card>
-                                <CardBody>
-                                    <CardTitle className="title">Card title</CardTitle>
-                                    <CardText>This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This card has even longer content than the first to
-                                        show that equal height action.</CardText>
-                                </CardBody>
-                            </Card>
+                            {
+                                this.state.notes.map((note) => {
+                                    return (
+                                        <Card onClick={() => {
+                                            console.log("Edit the card");
+                                            this.onEdit(true);
+                                        }}>
+                                            <CardBody>
+                                                <CardTitle className="title">Note-1</CardTitle>
+                                                <CardText>This is a wider card with supporting.</CardText>
+                                            </CardBody>
+                                        </Card>
+                                    )
+                                })
+                            }
+
                         </CardColumns>
                     }
+                    <Modal isOpen={this.state.edit} toggle={this.onEdit}>
+                        <ModalHeader className="modal-header">Modal title</ModalHeader>
+                        <ModalBody>
+                            <Input type="textarea"
+                                   className="modal-input"
+                                   value="Write something (data should remain in modal if unmountOnClose is set to false)"
+                                   rows={5}/>
+                            <br/>
+                            <div className="footer-label">
+                                <Label className="footer-edited">Edited 3:33 PM</Label>
+                            </div>
+                        </ModalBody>
+                        <ModalFooter className="modal-footer">
+                            <Button className="footer-button" onClick={this.onEdit}>Cancel</Button>{'     '}
+                            <Button className="footer-button" onClick={this.onEdit}>Save</Button>
+                        </ModalFooter>
+                    </Modal>
                 </div>
             </div>
         );
